cc1d0690535218d41e91cd2de16f13ea631d8c79
Millefeuille <mlabouri@student.42.fr>
2024-04-10 17:20:51 +0200

    removed blocking port forward

--- p3/scripts/run.sh
+++ p3/scripts/run.sh
@@ -6,7 +6,9 @@ kubectl apply -f /vagrant/kube/namespaces/argocd-namespace.yaml
 kubectl apply -f /vagrant/kube/namespaces/dev-namespace.yaml 
 
 echo "[INFO] -- Deploying argocd"
+kubectl apply -n argocd -f /vagrant/kube/configmaps/argocd-cmd-params-cm.yaml
 kubectl apply -n argocd -f /vagrant/kube/vendors/argocd.yaml
+# kubectl apply -n argocd -f /vagrant/kube/ingress/argocd-ingress.yaml
 
 echo "[INFO] -- Waiting for argocd to be ready"
 until kubectl wait pod --all --for=condition=Ready --namespace=argocd; do
@@ -17,5 +19,8 @@ done
 echo "[INFO] -- Getting initial password"
 argocd admin initial-password -n argocd
 
-echo "[INFO] -- Port forwarding"
-kubectl port-forward --address 0.0.0.0 svc/argocd-server -n argocd 8080:443
+echo "[INFO] -- Creating port forward service"
+cp /vagrant/services/kubectl-port-forward.service /etc/systemd/system/kubectl-port-forward.service
+systemctl daemon-reload
+systemctl enable kubectl-port-forward
+systemctl start kubectl-port-forward
