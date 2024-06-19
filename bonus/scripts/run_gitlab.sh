6cb6255602d47e2c2bb9773ff491fbc2567f32e8
Millefeuille <mlabouri@student.42.fr>
2024-06-19 18:14:44 +0200

    fix: minor performance improvements

--- bonus/scripts/run_gitlab.sh
+++ bonus/scripts/run_gitlab.sh
@@ -11,15 +11,10 @@ helm upgrade --install gitlab gitlab/gitlab \
   --set certmanager-issuer.email=me@bonus.iot.com \
   --set global.edition=ce \
   --set global.hosts.https="false" \
-  --set gitlab-runner.install=false \
-#  --set postgresql.image.tag=13.6.0 \
-#  --set global.ingress.configureCertmanager="false" \
+  --set gitlab-runner.install=false
 
 echo "[INFO] -- waiting for gitlab to start"
 kubectl rollout status deployment gitlab-webservice-default -n gitlab
-#until kubectl wait deployment gitlab-webservice-default --for=condition=available --namespace=gitlab --timeout=-1s; do
-#  sleep 1
-#done
 
 echo "[INFO] -- Creating port forward service"
 cp /vagrant/services/gitlab-port-forward.service /etc/systemd/system/gitlab-port-forward.service
