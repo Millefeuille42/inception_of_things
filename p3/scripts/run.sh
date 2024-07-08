e5ec08c4e7fa6c384d7993cb0dc244c692b7c02d
Millefeuille <mlabouri@student.42.fr>
2024-07-08 14:15:11 +0200

    fix(p3): removed extraneous wait

--- p3/scripts/run.sh
+++ p3/scripts/run.sh
@@ -8,16 +8,19 @@ kubectl apply -f /vagrant/kube/namespaces/dev-namespace.yaml
 echo "[INFO] -- Deploying argocd"
 kubectl apply -n argocd -f /vagrant/kube/vendors/argocd.yaml
 
-echo "[INFO] -- Waiting for argocd to be ready"
-kubectl rollout status deployment argocd-server -n argocd
-
-echo "[INFO] -- Creating argocd ressources"
+echo "[INFO] -- Configuring argocd for external access"
 kubectl apply -n argocd -f /vagrant/kube/configmaps/argocd-cmd-params-cm.yaml
 kubectl apply -n argocd -f /vagrant/kube/ingress/argocd-ingress.yaml
 
+echo "[INFO] -- Waiting for argocd to be ready"
 kubectl rollout restart deployment argocd-server -n argocd
 kubectl rollout status deployment argocd-server -n argocd
+until kubectl wait pod --all --for=condition=Ready --namespace=argocd; do
+  echo "waiting for pods to be created..."
+  sleep 1
+done
 
+echo "[INFO] -- Creating argocd ressources"
 kubectl apply -n argocd -f /vagrant/kube/projects/wilapp-project.yaml
 kubectl apply -n argocd -f /vagrant/kube/applications/wilapp-application.yaml
 
