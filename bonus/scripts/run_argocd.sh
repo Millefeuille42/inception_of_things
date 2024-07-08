42511689b7099a5ed3c85f7038709890e453ca83
Millefeuille <mlabouri@student.42.fr>
2024-07-08 15:36:10 +0200

    fix(bonus): removed use of service for glab and argocd in favor of ingress

--- bonus/scripts/run_argocd.sh
+++ bonus/scripts/run_argocd.sh
@@ -1,18 +1,17 @@
 echo "[INFO] -- Deploying argocd"
 kubectl apply -n argocd -f /vagrant/kube/vendors/argocd.yaml
 
+echo "[INFO] -- Configuring argocd for external access"
+kubectl apply -n argocd -f /vagrant/kube/configmaps/argocd-cmd-params-cm.yaml
+kubectl apply -n argocd -f /vagrant/kube/ingress/argocd-ingress.yaml
+
 echo "[INFO] -- Waiting for argocd to be ready"
+kubectl rollout restart deployment argocd-server -n argocd
 kubectl rollout status deployment argocd-server -n argocd
 until kubectl wait pod --all --for=condition=Ready --namespace=argocd; do
   echo "waiting for pods to be created..."
   sleep 1
 done
-
-echo "[INFO] -- Creating port forward service"
-cp /vagrant/services/argocd-port-forward.service /etc/systemd/system/argocd-port-forward.service
-systemctl daemon-reload
-systemctl enable argocd-port-forward
-systemctl start argocd-port-forward
 
 echo "[INFO] -- Creating argocd ressources"
 kubectl apply -n argocd -f /vagrant/kube/projects/wilapp-project.yaml
--- bonus/scripts/run_gitlab.sh
+++ bonus/scripts/run_gitlab.sh
@@ -6,9 +6,9 @@ helm repo update
 helm upgrade --install gitlab gitlab/gitlab \
   -n gitlab \
   --timeout 600s \
+  --set certmanager-issuer.email=me@bonus.iot.com \
   --set global.hosts.domain=$SERVER__IP \
   --set global.hosts.externalIP="$SERVER__IP" \
-  --set certmanager-issuer.email=me@bonus.iot.com \
   --set global.edition=ce \
   --set global.hosts.https="false" \
   --set gitlab-runner.install=false
@@ -16,11 +16,8 @@ helm upgrade --install gitlab gitlab/gitlab \
 echo "[INFO] -- waiting for gitlab to start"
 kubectl rollout status deployment gitlab-webservice-default -n gitlab
 
-echo "[INFO] -- Creating port forward service"
-cp /vagrant/services/gitlab-port-forward.service /etc/systemd/system/gitlab-port-forward.service
-systemctl daemon-reload
-systemctl enable gitlab-port-forward
-systemctl start gitlab-port-forward
+echo "[INFO] -- Configuring gitlab for external access"
+kubectl apply -n gitlab -f /vagrant/kube/ingress/gitlab-ingress.yaml
 
 echo "[INFO] -- retrieving gitlab password"
 kubectl get secret -n gitlab gitlab-gitlab-initial-root-password -o jsonpath='{.data.password}' | base64 -d; echo
